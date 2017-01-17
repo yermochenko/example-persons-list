49f56ca9973d935d90b536230aa1f1f526ab5d8d
Sergey Yermochenko <yermochenko@gmail.com>
2017-01-17 19:46:51 +0300

    Шаг 2

--- src/db/PersonStorage.java
+++ src/db/PersonStorage.java
@@ -23,22 +23,27 @@ public class PersonStorage {
     public List<Person> readAll() throws SQLException {
         String sql = "SELECT `id`, `first_name`, `middle_name`, `last_name`, `height`, `weight`, `is_citizen` FROM `person`";
         Connection c = getConnection();
-        Statement s = c.createStatement();
-        ResultSet r = s.executeQuery(sql);
-        List<Person> persons = new ArrayList<>();
-        while(r.next()) {
-            Person person = new Person();
-            person.setId(r.getInt("id"));
-            person.setFirstName(r.getString("first_name"));
-            person.setMiddleName(r.getString("middle_name"));
-            person.setLastName(r.getString("last_name"));
-            person.setHeight(r.getDouble("height"));
-            person.setWeight(r.getDouble("weight"));
-            person.setCitizen(r.getBoolean("is_citizen"));
-            persons.add(person);
+        Statement s = null;
+        ResultSet r = null;
+        try {
+            s = c.createStatement();
+            r = s.executeQuery(sql);
+            List<Person> persons = new ArrayList<>();
+            while(r.next()) {
+                Person person = new Person();
+                person.setId(r.getInt("id"));
+                person.setFirstName(r.getString("first_name"));
+                person.setMiddleName(r.getString("middle_name"));
+                person.setLastName(r.getString("last_name"));
+                person.setHeight(r.getDouble("height"));
+                person.setWeight(r.getDouble("weight"));
+                person.setCitizen(r.getBoolean("is_citizen"));
+                persons.add(person);
+            }
+            return persons;
+        } finally {
+            try { r.close(); } catch(NullPointerException | SQLException e) {}
+            try { s.close(); } catch(NullPointerException | SQLException e) {}
         }
-        r.close();
-        s.close();
-        return persons;
     }
 }
